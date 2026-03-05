49e5e6eb0e7c72f944e7d83287a2daf8ce2fd88f
tanvir-wd22 <tanvirwd22@gmail.com>
2026-03-06 01:09:45 +0600

    alhamdulliah lootie react and firebase setup done

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,61 @@
+import Lottie from 'lottie-react';
+import registerAnimation from '../assets/register.json';
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const handleRegisterForm = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const photo = e.target.photo.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, photo, email, password);
+  };
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="w-96">
+            <Lottie animationData={registerAnimation}></Lottie>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleRegisterForm} className="card-body">
+              <fieldset className="fieldset">
+                <label className="label">Name</label>
+                <input
+                  name="name"
+                  type="text"
+                  className="input"
+                  placeholder="Name"
+                />
+                <label className="label">Photo</label>
+                <input
+                  name="photo"
+                  type="text"
+                  className="input"
+                  placeholder="Photo"
+                />
+                <label className="label">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
